b17e8d9631621b9670dc087c4ef26d7ae16bbdf5
thebestjosh <jzhang27@punahou.edu>
2026-02-17 16:26:46 -1000

    Fix shooter

--- 2026-FRC-2090/src/main/java/frc/robot/RobotContainer.java
+++ 2026-FRC-2090/src/main/java/frc/robot/RobotContainer.java
@@ -201,7 +201,7 @@ public class RobotContainer {
       driverXbox.back().whileTrue(Commands.none());
       driverXbox.leftBumper().whileTrue(Commands.runOnce(drivebase::lock, drivebase).repeatedly());
       driverXbox.rightBumper().onTrue(Commands.none());
-      driverXbox.a().onTrue(shooter.runMotors());
+      driverXbox.a().whileTrue(shooter.runMotors());
     }
 
   }
